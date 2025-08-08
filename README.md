477e1ee11356ff51ec3f66358596e9ec93bb3ad5
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2025-08-08 13:57:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# File Vault
+
+Basic Flutter app scaffolded for desktop (Windows, Linux, macOS) and Web using Material 3.
+
+## Run
+
+- Windows: `flutter run -d windows`
+- Linux: `flutter run -d linux`
+- macOS: `flutter run -d macos`
+- Web: `flutter run -d chrome`
+
+If a platform device isn't listed, ensure it's enabled:
+
+```
+flutter config --enable-windows-desktop --enable-linux-desktop --enable-macos-desktop --enable-web
+```
+
+## Notes
+
+- Material 3 is enabled via `ThemeData(useMaterial3: true)`.
+- Replace the placeholder home screen with your app content.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'File Vault',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'File Vault'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Placeholder state for future features.
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to File Vault',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
